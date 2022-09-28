40ce4ec21a8a59e3cc9a0ecd7c14d85d582ddd94
roy-276 <maingiroy95@gmail.com>
2022-09-28 07:59:21 +0300

    Updated-Seed-Sample-Users

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -8,7 +8,6 @@ const seedRouter = express.Router();
 seedRouter.get('/', async (req, res) => {
   await Product.remove({});
   const createdProducts = await Product.insertMany(data.products);
-  res.send({ createdProducts });
   await User.remove({});
   const createdUsers = await User.insertMany(data.users);
   res.send({ createdProducts, createdUsers });
