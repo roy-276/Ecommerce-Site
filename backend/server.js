fbdd58a513308a2f878c31733055ca7c5aaf132c
roy-276 <maingiroy95@gmail.com>
2022-10-07 08:36:28 +0300

    OrderHistoryScreen

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,10 @@ app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 
+app.get('api/keys/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
+
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
